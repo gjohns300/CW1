3a69cf4e99d457d2c7f2a4caa3d1c5cd299b698f
“gjohns300” <gjohns300@caledonian.ac.uk>
2022-11-17 18:39:37 +0000

    UnitTestPush

--- UnitTests.sh
+++ UnitTests.sh
@@ -2,10 +2,11 @@
 #--------UNIT TEST 1----------
 #setting java value to testInput1 to be tested
 input=$"1 pounds"
-actualOutput=$(java CurrencyConverter $testInput)
+actualOutput=$(java CurrencyConverter $input)
+echo "$actualOutput"
 
 #checking if testInput is empty
-if [ -z "$actualInput" ];
+if [ -z "$input" ];
 #test failed display message
 then    echo "Test Failed"
 exit 1
@@ -15,7 +16,7 @@ fi
 
 #--------UNIT TEST 2---------
 #checking if testInput contains capital letters
-if [[ "$actualInput" =~ [A-Z] ]];
+if [[ "$input" =~ [A-Z] ]];
 #test failed display message
 then    echo "Test Failed"
 exit 1
@@ -24,17 +25,47 @@ else    echo "Test Passed"
 fi
 
 
-
-
 #--------UNIT TEST 3---------
-#checking if testInput contains capital letters
-expectdOutput=$"1.0 Pounds = 2.36 Dollars 1.0 Pound = 1.19 Euros"
-if [[ "$actualOutput" == "$expectedOutput" ]];
+#checking if input contains capital letters
+expectedOutcome1="1.0 Dollars = 0.74 Pounds
+1.0 Dollars = 0.88 Euros
+Thank you for using the converter."
+
+input=$"1 dollars"
+actualOutput=$(java CurrencyConverter $input)
+
+
+if [[ "$actualOutput" == "$expectedOutcome1" ]];
+#test passed message
+then    echo "Test Passed"
+else    echo "Test Failed"
+fi
+
+input=$"1 pounds"
+actualOutput=$(java CurrencyConverter $input)
+
+
+expectedOutcome2="1.0 Pounds = 1.36 Dollars
+1.0 Pounds = 1.19 Euros
+Thank you for using the converter."
+
+if [[ "$actualOutput" == "$expectedOutcome2" ]];
 #test passed message
 then    echo "Test Passed"
 else	echo "Test Failed"
-exit 1
 fi
 
+input=$"1 euros"
+actualOutput=$(java CurrencyConverter $input)
+
+expectedOutcome3="1 Euros = 1.19 Dollars
+1.0 Euros = 0.84 Pounds
+Thank you for using the converter."
+
+if [[ "$actualOutput" == "$expectedOutcome3" ]];
+#test passed message
+then    echo "Test Passed"
+else    echo "Test Failed"
+fi
 
 
