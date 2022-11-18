0dd9db7daef24c0caaadde5928e7b4d27a89bca7
“gjohns300” <gjohns300@caledonian.ac.uk>
2022-11-18 16:42:42 +0000

    UT

--- UnitTests.sh
+++ UnitTests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #--------UNIT TEST 1----------
 #setting java value to input to be tested
+echo "-------Unit Test 1------"
+
 actualOutput=$(java CurrencyConverter)
 expectedOutput=$"No input detected. Please input your amount and then currency"
-
-#checking if inputs match
-if [[ "actualOutput" == "expectedOutput" ]];
+#checking if outputs match
+if [[ "$actualOutput" == "$expectedOutput" ]];
 #user notification display message
 #test passed message
 then	echo "Checking for empty value - Test Passed"
@@ -14,47 +15,47 @@ else    echo "Checking for empty value - Test Failed"
 fi
 
 #checking is last character is a number. If it is test fails due to wrong formatting
-inputLastChar=${input: -1}
-if [[ $inputLastChar =~ [0-9] ]];
-then	echo "Incorrect format of input. Please enter amount of currency before type"
+input=$"dollars 1"
+actualOutput=$(java CurrencyConverter $input)
+expectedOutput=$"Wrong format entered
+1.0 Dollars = 0.74 Pounds
+1.0 Dollars = 0.88 Euros
+Thank you for using the converter."
+
+echo $actualOutput
+if [[ "$actualOutput" == "$expectedOutput" ]];
+then	echo "Checking for correct format - Test Passed"
 #test passed message
-else 	echo "Checking for correct format - Test Passed"
+else 	echo "Incorrect format of input. Please enter amount of currency before type"
+
 fi
 
 #--------UNIT TEST 2---------
+echo "-------Unit Test 2------"
 
-input=$"50 euros"
 
-#checking if input contains capital letters
-if [[ "$input" =~ [[:upper:]] ]];
-#contains capital yes message
-then    echo "Checking for capital letter - Yes"
-#contains capital no message
-else    echo "Checking for capital letter - No"
-fi
-
-input=$"50 POUNDS"
+input=$"1 DOLLARS"
+actualOutput=$(java CurrencyConverter $input)
+expectedOutput=$"1.0 Dollars = 0.74 Pounds
+1.0 Dollars = 0.88 Euros
+Thank you for using the converter."
 
 #checking if input contains capital letters
-if [[ "$input" =~ [[:upper:]] ]];
-#contains capital yes message
-then    echo "Checking for capital letter - Yes"
-#contains capital no message
-else    echo "Checking for capital letter - No"
-fi
-
-input=$"50 Dollars"
-
-#checking if input contains capital letters
-if [[ "$input" =~ [[:upper:]] ]];
-#contains capital yes message
-then    echo "Checking for capital letter - Yes"
-#contains capital no message
-else    echo "Checking for capital letter - No"
+#checking if outputs match
+if [[ "$actualOutput" == "$expectedOutput" ]];
+#user notification display message
+#test passed message
+then    echo "Checking for conversion value - Test Passed"
+#test failed message
+else    echo "Checking for empty value - Test Failed"
 fi
 
 
 #--------UNIT TEST 3---------
+
+echo "-------Unit Test 3------"
+
+
 #checking if dollar conversion works
 expectedOutcome1="1.0 Dollars = 0.74 Pounds
 1.0 Dollars = 0.88 Euros
@@ -62,7 +63,6 @@ Thank you for using the converter."
 
 input=$"1 dollars"
 actualOutput=$(java CurrencyConverter $input)
-
 
 if [[ "$actualOutput" == "$expectedOutcome1" ]];
 #test passed message
