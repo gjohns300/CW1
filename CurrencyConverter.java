9cda3f43d71bd3d5ec516d37dab0c024ef1d230d
“gjohns300” <gjohns300@caledonian.ac.uk>
2022-11-18 13:15:33 +0000

    Currency

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -5,7 +5,7 @@ public class CurrencyConverter {
 
         public static void main(String[] args) {
 
-		if(args == null)
+		if(args.length == 0)
 		{
 			System.out.println("No input detected. Please input your amount and then currency");
 			System.exit(0);
